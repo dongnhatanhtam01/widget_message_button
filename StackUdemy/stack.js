6da2a40a476c7aaf58ca9aebb802727f6ce1126f
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-07-26 01:59:40 +0700

    Stack pop

--- StackUdemy/stack.js
+++ StackUdemy/stack.js
@@ -11,6 +11,20 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+
+  /** The function should accept a value
+   * Create a new node with that value
+   * 
+   * If there are no nodes in the stack, set the first and last property to be the newly created node
+   * 
+   * If there is at least one node, create a variable that stores the current first property on the stack
+   * 
+   * Reset the first property to be a newly create node
+   * 
+   * Set the next property on the node to be the previously created variable - temp
+   * 
+   * Increment the size of the stack by 1
+   */
   push(val){
     var newNode = new Node(val);
     if(!this.first) {
@@ -21,10 +35,30 @@ class Stack {
       var temp = this.first;
       this.first = newNode;
       this.first.next = temp;
-
-
     }
     return ++this.size;
+  }
+
+  /** If there is no node in stack, return null
+   * 
+   * Create a temporary variable to store the first property on the stack
+   * 
+   * If there is only one node, set the first an last property to be null
+   * 
+   * If there is more than one node, set the first property to be the next property on the current first
+   * 
+   * Decrement the size by 1
+   * 
+   */
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size --;
+    return temp.value;
   }
 
 }
